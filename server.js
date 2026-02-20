6dfe3203a3d1acd2c5c554f8d08b71338c57799e
Hussnain Ashiq <hussnainnaz3@gmail.com>
2026-02-20 12:21:56 +0500

    Node Js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.set('view engine','ejs')
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.listen(PORT, () => {
+  console.log(`Your Project is Running on PORT Number ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World Hussnains',
+  });
+});
+
+app.get('/user',(req,res)=>{
+    res.render('user')
+})
+
+app.get('/about', (req, res) => {
+  const fileUrl = "http://localhost:3000/files/Project%20Report.docx";
+  res.redirect(`https://docs.google.com/gview?url=${fileUrl}&embedded=true`);
+});
+
+app.get('/abouts', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Files', 'ali.pdf'));
+});
+
+
+app.post('/submit',(req,res)=>{
+    res.send(req.body);
+})
