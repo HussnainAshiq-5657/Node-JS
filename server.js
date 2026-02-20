62e77f345f1e49051f3ee2a05128759f6f18d50a
Hussnain Ashiq <hussnainnaz3@gmail.com>
2026-02-20 19:48:12 +0500

    Added the EJA

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Your Project is Running on PORT Number ${PORT}`);
@@ -17,12 +18,12 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/user',(req,res)=>{
-    res.render('user')
-})
+app.get('/user', (req, res) => {
+  res.render('user');
+});
 
 app.get('/about', (req, res) => {
-  const fileUrl = "http://localhost:3000/files/Project%20Report.docx";
+  const fileUrl = 'http://localhost:3000/files/Project%20Report.docx';
   res.redirect(`https://docs.google.com/gview?url=${fileUrl}&embedded=true`);
 });
 
@@ -30,7 +31,38 @@ app.get('/abouts', (req, res) => {
   res.sendFile(path.join(__dirname, 'Files', 'ali.pdf'));
 });
 
+app.post('/submit', (req, res) => {
+  res.send(req.body);
+});
 
-app.post('/submit',(req,res)=>{
-    res.send(req.body);
-})
+app.get('/gallery', (req, res) => {
+  const Items = ['Hussnain', 'Ali', 'Hassan', 'Ali'];
+  const data = [
+    {
+      name: 'Hussnain',
+      age: 22,
+      university: 'UET',
+    },
+    {
+      name: 'Hussnain',
+      age: 22,
+      university: 'UET',
+    },
+    {
+      name: 'Hussnain',
+      age: 22,
+      university: 'UET',
+    },
+    {
+      name: 'Hussnain',
+      age: 22,
+      university: 'UET',
+    },
+  ];
+  res.render('Gallery', {
+    title: 'Gallery Page',
+    message: ' Lorem ipsum, dolor sit ',
+    items: Items,
+    data
+  });
+});
